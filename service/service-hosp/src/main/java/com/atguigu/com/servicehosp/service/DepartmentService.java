e3f88b9c7a77889b30ea14939e1f5fa506e0a495
gen_l <962317092@qq.com>
2023-06-17 15:25:12 +0800

    DepartmentVo

--- service/service-hosp/src/main/java/com/atguigu/com/servicehosp/service/DepartmentService.java
+++ service/service-hosp/src/main/java/com/atguigu/com/servicehosp/service/DepartmentService.java
@@ -2,8 +2,10 @@ package com.atguigu.com.servicehosp.service;
 
 import com.atguigu.com.model.model.hosp.Department;
 import com.atguigu.com.model.vo.hosp.DepartmentQueryVo;
+import com.atguigu.com.model.vo.hosp.DepartmentVo;
 import org.springframework.data.domain.*;
 
+import java.util.List;
 import java.util.Map;
 
 public interface DepartmentService {
@@ -11,4 +13,6 @@ public interface DepartmentService {
 
     Page<Department> selectPage(Integer page, Integer limit, DepartmentQueryVo departmentQueryVo);
 
+    //根据医院编号，查询医院所有科室列表
+    List<DepartmentVo> findDeptTree(String hoscode);
 }
